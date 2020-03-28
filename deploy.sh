be6f53b060cc97f8650064c0a4300642ca83bbb8
zhouatie <1114349984@qq.com>
2020-03-29 01:48:39 +0800

    feat: update

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run docs:build
 
 # 进入生成的文件夹
-cd /build
+cd ./build
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:zhouatie/zhouatie.github.io.git master
+git push -f git@github.com:zhouatie/zhouatie.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zhouatie/atie-doc.git master:gh-pages
+# git push -f git@github.com:zhouatie/atie-doc.git master:gh-pages
 
 cd -
